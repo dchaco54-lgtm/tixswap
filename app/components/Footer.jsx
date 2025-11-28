c97a0afceaf4a6187eefa3324f1c37ec723c5c57
dchaco54-lgtm <dchaco54@gmail.com>
2025-11-27 21:49:07 -0300

    Delete app directory

--- app/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="py-10 border-t border-gray-800 text-center text-gray-400">
-      <p>TixSwap © {new Date().getFullYear()} — Todos los derechos reservados.</p>
-    </footer>
-  );
-}
