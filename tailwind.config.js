e4a3f6b8a01299286e44345db2de569f990236df
dchaco54-lgtm <dchaco54@gmail.com>
2025-11-29 15:06:28 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: [
     "./app/**/*.{js,jsx,ts,tsx,mdx}",
+    "./app/components/**/*.{js,jsx,ts,tsx,mdx}",
+    "./app/events/**/*.{js,jsx,ts,tsx,mdx}",
+    "./app/[id]/**/*.{js,jsx,ts,tsx,mdx}",
   ],
   theme: {
     extend: {},
