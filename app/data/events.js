1c8436c2035ad41ec3565445023f4d7dff87dba0
dchaco54-lgtm <dchaco54@gmail.com>
2025-11-29 13:18:14 -0300

    Create events.js

---
+++ app/data/events.js
@@ -0,0 +1,188 @@
+// data/events.js
+
+export const events = [
+  // --- ENERO ---
+  {
+    id: "my-chemical-romance",
+    name: "My Chemical Romance",
+    date: "2026-01-29T21:00:00",
+    location: "Estadio Bicentenario de La Florida",
+    category: "Rock",
+  },
+
+  // --- FEBRERO ---
+  {
+    id: "chayanne-concepcion",
+    name: "Chayanne",
+    date: "2026-02-07T21:00:00",
+    location: "Concepción",
+    category: "Pop Latino",
+  },
+  {
+    id: "doja-cat",
+    name: "Doja Cat",
+    date: "2026-02-10T21:00:00",
+    location: "Movistar Arena",
+    category: "Hip Hop",
+  },
+  {
+    id: "chayanne-santiago",
+    name: "Chayanne",
+    date: "2026-02-11T21:00:00",
+    location: "Estadio Nacional de Santiago",
+    category: "Pop Latino",
+  },
+  {
+    id: "galneryus",
+    name: "GALNERYUS",
+    date: "2026-02-13T21:00:00",
+    location: "Teatro Cariola",
+    category: "Metal",
+  },
+  {
+    id: "chayanne-vina",
+    name: "Chayanne",
+    date: "2026-02-14T21:00:00",
+    location: "Quinta Vergara, Viña del Mar",
+    category: "Pop Latino",
+  },
+  {
+    id: "jason-mraz",
+    name: "Jason Mraz",
+    date: "2026-02-25T21:00:00",
+    location: "Teatro Caupolicán",
+    category: "Pop",
+  },
+  {
+    id: "alejandro-sanz",
+    name: "Alejandro Sanz",
+    date: "2026-02-28T21:00:00",
+    location: "Movistar Arena",
+    category: "Pop",
+  },
+
+  // --- MARZO ---
+  {
+    id: "lollapalooza-2026",
+    name: "Lollapalooza Chile 2026",
+    date: "2026-03-13T12:00:00",
+    location: "Parque O'Higgins",
+    category: "Festival",
+  },
+  {
+    id: "acdc-1",
+    name: "AC/DC",
+    date: "2026-03-11T21:00:00",
+    location: "Parque Estadio Nacional",
+    category: "Rock",
+  },
+  {
+    id: "acdc-2",
+    name: "AC/DC",
+    date: "2026-03-15T21:00:00",
+    location: "Parque Estadio Nacional",
+    category: "Rock",
+  },
+  {
+    id: "soda-stereo",
+    name: "Soda Stereo",
+    date: "2026-03-26T21:00:00",
+    location: "Movistar Arena",
+    category: "Rock Latino",
+  },
+  {
+    id: "pennywise",
+    name: "Pennywise",
+    date: "2026-03-20T21:00:00",
+    location: "Teatro Caupolicán",
+    category: "Punk",
+  },
+  {
+    id: "kali-uchis",
+    name: "Kali Uchis",
+    date: "2026-03-11T21:00:00",
+    location: "Parque Deportivo Estadio Nacional",
+    category: "Pop",
+  },
+
+  // --- ABRIL ---
+  {
+    id: "laura-pausini-1",
+    name: "Laura Pausini",
+    date: "2026-04-15T21:00:00",
+    location: "Movistar Arena",
+    category: "Pop",
+  },
+  {
+    id: "laura-pausini-2",
+    name: "Laura Pausini",
+    date: "2026-04-16T21:00:00",
+    location: "Movistar Arena",
+    category: "Pop",
+  },
+  {
+    id: "mac-demarco-1",
+    name: "Mac DeMarco",
+    date: "2026-04-18T21:00:00",
+    location: "Teatro Caupolicán",
+    category: "Indie",
+  },
+  {
+    id: "mac-demarco-2",
+    name: "Mac DeMarco",
+    date: "2026-04-19T21:00:00",
+    location: "Teatro Caupolicán",
+    category: "Indie",
+  },
+  {
+    id: "ysy-a",
+    name: "Ysy A",
+    date: "2026-04-01T21:00:00",
+    location: "Espacio Riesco",
+    category: "Trap",
+  },
+  {
+    id: "respira-festival",
+    name: "Respira Festival",
+    date: "2026-04-03T21:00:00",
+    location: "Movistar Arena",
+    category: "Festival",
+  },
+  {
+    id: "red-bull-batalla",
+    name: "Red Bull Batalla - Final Internacional 2026",
+    date: "2026-04-11T21:00:00",
+    location: "Movistar Arena",
+    category: "Freestyle",
+  },
+  {
+    id: "black-label-society",
+    name: "Black Label Society",
+    date: "2026-04-30T21:00:00",
+    location: "Teatro Coliseo",
+    category: "Metal",
+  },
+  {
+    id: "ill-nino",
+    name: "Ill Niño",
+    date: "2026-04-26T21:00:00",
+    location: "Teatro Cariola",
+    category: "Nu Metal",
+  },
+  {
+    id: "feuerschwanz",
+    name: "Feuerschwanz",
+    date: "2026-04-21T21:00:00",
+    location: "Teatro Cariola",
+    category: "Folk Metal",
+  },
+
+  // --- MAYO ---
+  {
+    id: "pimpinela",
+    name: "Pimpinela",
+    date: "2026-05-17T21:00:00",
+    location: "Movistar Arena",
+    category: "Romántico",
+  },
+];
