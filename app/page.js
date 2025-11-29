6118e63a9e1b5603b86422660dd74cd8e0578273
dchaco54-lgtm <dchaco54@gmail.com>
2025-11-29 15:02:35 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,45 +1,16 @@
-export const dynamic = "force-dynamic";
-
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Categories from "./components/Categories";
-import EventGrid from "./components/EventGrid";
-import CTA from "./components/CTA";
-import Footer from "./components/Footer";
+import Hero from "../components/Hero";
+import Categories from "../components/Categories";
+import EventGrid from "../components/EventGrid";
+import CTA from "../components/CTA";
 
 export default function Home() {
-  const featuredEvents = [
-    {
-      id: 1,
-      title: "My Chemical Romance",
-      category: "Rock",
-      date: "29 de enero de 2026",
-      location: "Estadio Bicentenario La Florida",
-    },
-    {
-      id: 2,
-      title: "Chayanne",
-      category: "Pop Latino",
-      date: "7 de febrero de 2026",
-      location: "Concepción",
-    },
-    {
-      id: 3,
-      title: "Doja Cat",
-      category: "Hip Hop",
-      date: "10 de febrero de 2026",
-      location: "Movistar Arena",
-    },
-  ];
-
   return (
-    <main>
-      <Header />
+    <>
       <Hero />
       <Categories />
-      <EventGrid events={featuredEvents} />
+      <EventGrid />
       <CTA />
-      <Footer />
-    </main>
+    </>
   );
 }
+
