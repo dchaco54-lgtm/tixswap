2a5b23fc5275c5b1c41818af50f529ee02c38cd0
dchaco54-lgtm <dchaco54@gmail.com>
2025-11-29 15:25:38 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,16 +1,24 @@
-import Hero from "../components/Hero";
-import Categories from "../components/Categories";
-import EventGrid from "../components/EventGrid";
-import CTA from "../components/CTA";
+import Hero from "./components/Hero";
+import Categories from "./components/Categories";
+import EventGrid from "./components/EventGrid";
+import CTA from "./components/CTA";
+import { events } from "./data/events";
 
 export default function Home() {
   return (
-    <>
+    <div className="space-y-20">
       <Hero />
+
+      {/* Categorías */}
       <Categories />
-      <EventGrid />
+
+      {/* Eventos Destacados */}
+      <section className="py-16">
+        <h2 className="text-3xl font-bold mb-8">Eventos destacados de Chile</h2>
+        <EventGrid events={events} />
+      </section>
+
       <CTA />
-    </>
+    </div>
   );
 }
-
