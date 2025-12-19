f1db593325ec8c508aff9e27ce4dd9658a1f374b
dchaco54-lgtm <dchaco54@gmail.com>
2025-12-19 20:08:39 -0300

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -2,4 +2,5 @@
 /// <reference types="next/image-types/global" />
 
 // NOTE: This file should not be edited.
-// See https://nextjs.org/docs/basic-features/typescript for more information.
+// See https://nextjs.org/docs/pages/building-your-application/configuring/typescript for more information.
+
