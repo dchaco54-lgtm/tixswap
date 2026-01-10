1121d0b98f6dbdf08caaf8758cde575ceaed263c
dchaco54-lgtm <dchaco54@gmail.com>
2026-01-10 17:11:22 -0300

    Create ticketCard.jsx

---
+++ components/ticketCard.jsx
@@ -0,0 +1,4 @@
+// Re-export/compat layer for older imports like: import TicketCard from "@/components/ticketCard"
+// The canonical implementation lives in /app/components/ticketCard.jsx
+
+export { default } from "@/app/components/ticketCard";
