58c40947731753f18c095a6b309dad0263e373ce
dchaco54-lgtm <dchaco54@gmail.com>
2026-01-13 00:05:27 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
+
+// Allow remote images (Supabase Storage, Imgur, Unsplash, Google) so event cards can render images
+// without breaking in production.
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+let supabaseHostname;
+try {
+  supabaseHostname = supabaseUrl ? new URL(supabaseUrl).hostname : undefined;
+} catch {
+  supabaseHostname = undefined;
+}
+
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      ...(supabaseHostname ? [{ protocol: 'https', hostname: supabaseHostname }] : []),
+      { protocol: 'https', hostname: 'i.imgur.com' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+    ],
+  },
 };
 
 module.exports = nextConfig;
