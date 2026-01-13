143ce779df7c955e4d79d540b8a9130f9f4ec6b1
dchaco54-lgtm <dchaco54@gmail.com>
2026-01-12 22:50:35 -0300

    Update supabaseClient.js

--- app/lib/supabaseClient.js
+++ app/lib/supabaseClient.js
@@ -1,2 +1,6 @@
 // app/lib/supabaseClient.js
+// Este wrapper existe para imports relativos desde /app.
+// OJO: `export *` NO re-exporta el default, por eso supabase quedaba undefined.
+
+export { default } from "../../lib/supabaseClient";
 export * from "../../lib/supabaseClient";
