0fbca1710425a9718e6c05078ca451bc5562a695
dchaco54-lgtm <dchaco54@gmail.com>
2026-01-19 11:11:20 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -19,6 +19,32 @@ const nextConfig = {
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
     ],
   },
+  
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'geolocation=(), microphone=(), camera=()',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
