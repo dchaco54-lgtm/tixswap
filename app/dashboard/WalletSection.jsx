9402a37b4589ee906366502ecc01b5c28659815a
dchaco54-lgtm <dchaco54@gmail.com>
2026-01-28 12:30:38 -0300

    Correccion wallet

--- app/dashboard/WalletSection.jsx
+++ app/dashboard/WalletSection.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { createClient } from "@/lib/supabase/client";
 
